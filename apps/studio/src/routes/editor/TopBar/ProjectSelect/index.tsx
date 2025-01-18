fc38dbc6dd0d8ddfa5f4779ccd279e144953ecc4
JuliusWallblom <juliuswallblom@gmail.com>
2025-01-18 22:22:32 +0100

    Icon change.

--- apps/studio/src/routes/editor/TopBar/ProjectSelect/index.tsx
+++ apps/studio/src/routes/editor/TopBar/ProjectSelect/index.tsx
@@ -66,7 +66,9 @@ const ProjectBreadcrumb = observer(() => {
                             className="mx-0 px-0 text-foreground-onlook text-small hover:text-foreground-active hover:bg-transparent"
                             onClick={handleReturn}
                         >
-                            <Icons.OnlookLogo className="w-6 h-6 mr-2 hidden md:block" />
+                            <div className="mr-1.5">
+                                <Icons.AstralIcon className="hidden md:block" />
+                            </div>
                             {'Astral'}
                         </Button>
                     </TooltipTrigger>
